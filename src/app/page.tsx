f20e473b94c1f04b76376c858a604e201a8d8c2e
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-11-16 10:42:50 +0200

    Improve error messages for environment variable checks in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  if(process.env.NO_COMMENT != '123456789') throw new Error('Enviroment variable is parsed incorrectly')
-  if(process.env.WITH_COMMENT != '123456789') throw new Error('Enviroment variable is parsed incorrectly')
+  if(process.env.NO_COMMENT != '123456789') throw new Error('Enviroment variable process.env.NO_COMMENT is parsed incorrectly')
+  if(process.env.WITH_COMMENT != '123456789') throw new Error('Enviroment variable process.env.WITH_COMMENT is parsed incorrectly')
   return <div>Test</div>;
 }
